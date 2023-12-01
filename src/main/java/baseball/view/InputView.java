d6a4f774f1d718d991bc3ddaf7ead1b1a56a5445
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-12-01 13:45:35 +0900

    feat: 사용자 입력 받는 InputView 클래스 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,14 +3,17 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    public static void printGameStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 
     public static String getUserInput() {
-        System.out.println("숫자를 입력해주세요 : ");
-        return Console.readLine();
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine().trim();
     }
 
     public static String getRestartInput() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        return Console.readLine();
+        return Console.readLine().trim();
     }
 }
