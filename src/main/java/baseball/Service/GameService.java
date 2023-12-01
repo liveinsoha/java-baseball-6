a263536762e2283136d9a2e5822b354f6ad41e12
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-12-01 13:46:44 +0900

    feat: 게임 진행 클래스 구현

--- src/main/java/baseball/Service/GameService.java
+++ src/main/java/baseball/Service/GameService.java
@@ -1,2 +1,29 @@
-package baseball.Service;public class GameService {
+package baseball.Service;
+
+import baseball.domain.Computer;
+import baseball.domain.Input;
+import baseball.domain.Result;
+import baseball.util.RandomGenerator;
+import baseball.view.InputView;
+
+public class GameService {
+
+    Computer computer = new Computer(new RandomGenerator());
+
+    public void startGame() {
+        computer.generateAnswer();
+    }
+
+    public Result playRound() {
+        Input input = Input.of(InputView.getUserInput());
+        return getResultOfRound(input);
+    }
+
+    private Result getResultOfRound(Input input) {
+        return Result.of(computer.calculateStrikeCount(input), computer.calculateBallCount(input));
+    }
+
+    public boolean isGameOver(Result result) {
+        return result.isThreeStrike();
+    }
 }
